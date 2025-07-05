3683109d2341944c2ce55eb4e288f943dc164a1b
moisseev <moisseev@users.noreply.github.com>
2025-07-05 09:45:02 +0000

    Deploying to gh-pages from @ moisseev/docs.rspamd.com@21001d8804a5998ec76f2da426e4a474245ce9d2 🚀

---
+++ pr/2/assets/js/6e46f66b.04936c80.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkrspamd_docs=self.webpackChunkrspamd_docs||[]).push([[7994],{26732:e=>{e.exports=JSON.parse('{"tags":[{"label":"performance","permalink":"/docs.rspamd.com/branches/2/merge/blog/tags/performance","count":1},{"label":"benchmarks","permalink":"/docs.rspamd.com/branches/2/merge/blog/tags/benchmarks","count":1}]}')}}]);
