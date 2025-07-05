7195dfc428ab4a34f4ef850a59fb72dd0d015637
moisseev <moisseev@users.noreply.github.com>
2025-07-05 14:01:14 +0000

    Deploying to gh-pages from @ moisseev/docs.rspamd.com@5bbec0fccc3607f47dce25ce18051cd4ce0384ad 🚀

---
+++ pr/5/ru/assets/js/ce75db0b.6da10dfc.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkrspamd_docs=self.webpackChunkrspamd_docs||[]).push([[6548],{2332:s=>{s.exports=JSON.parse('{"tags":[{"label":"performance","permalink":"/docs.rspamd.com/pr/5/ru/blog/tags/performance","count":1},{"label":"benchmarks","permalink":"/docs.rspamd.com/pr/5/ru/blog/tags/benchmarks","count":1}]}')}}]);
