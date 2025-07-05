8ef73bf0fd5e185dc2e0b8f174536e06977b73e2
moisseev <moiseev@mezonplus.ru>
2025-07-05 14:55:53 +0000

    Deploying to gh-pages from @ moisseev/docs.rspamd.com@8d007a32561824b71bfee7b6d6d3212d69be2918 🚀

---
+++ branches/test-pr6/assets/js/892ea0a5.763659a2.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkrspamd_docs=self.webpackChunkrspamd_docs||[]).push([[3760],{38849:s=>{s.exports=JSON.parse('{"tags":[{"label":"performance","permalink":"/docs.rspamd.com/branches/test-pr6/blog/tags/performance","count":1},{"label":"benchmarks","permalink":"/docs.rspamd.com/branches/test-pr6/blog/tags/benchmarks","count":1}]}')}}]);
