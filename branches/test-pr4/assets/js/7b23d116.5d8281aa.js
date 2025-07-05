4a4eb8245fc38b8daece15eff40e5ffe6bb7562a
moisseev <moiseev@mezonplus.ru>
2025-07-05 11:33:37 +0000

    Deploying to gh-pages from @ moisseev/docs.rspamd.com@6ee3eaefe557ab8a11574c94e83fd71d0293845a 🚀

---
+++ branches/test-pr4/assets/js/7b23d116.5d8281aa.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkrspamd_docs=self.webpackChunkrspamd_docs||[]).push([[6865],{74095:s=>{s.exports=JSON.parse('{"metadata":{"permalink":"/docs.rspamd.com/branches/test-pr4/blog","page":1,"postsPerPage":5,"totalPages":1,"totalCount":1,"blogDescription":"News and updates from the Rspamd project","blogTitle":"Rspamd Blog"}}')}}]);
